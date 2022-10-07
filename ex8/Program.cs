ef8d66c7f01585f987697a205af59d42720822f7
Irina <pamir.08@mail.ru>
2022-10-07 17:28:13 +0300

    ex8

---
+++ ex8/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = new Random () .Next (10, 99);
+Console.WriteLine (number);
+
+int a = number % 10;
+int b = (number % 100 - number % 10) / 10;
+
+if (a > b)
+{
+ Console.WriteLine ("Наибольшая цифра числа: ");
+ Console. Write (a);   
+}
+
+else
+{
+ Console.WriteLine ("Наибольшая цифра числа: ");
+ Console. Write (b);   
+}
