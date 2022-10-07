8bd9da411c24ad637f1ebc79be4603583c76ac39
Irina <pamir.08@mail.ru>
2022-10-07 18:01:59 +0300

    ex9

---
+++ ex9/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random () .Next (100, 999);
+Console.WriteLine (number);
+
+int c = number % 10;
+int a = (number - number % 100) / 100;
+
+Console.Write (a);   
+Console.Write (c);  
