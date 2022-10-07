50d7995ea94a9bc768d673ecbaa15fbdfcb1735e
Irina <pamir.08@mail.ru>
2022-10-07 16:19:15 +0300

    ex4

---
+++ ex4/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = new Random () .Next (1, 1000);
+Console.WriteLine (a);
+
+if (a % 2 == 0 )
+{
+    Console.WriteLine ("Это четное число");
+}
+else
+{
+    Console.WriteLine ("Это нечетное число");
+}
