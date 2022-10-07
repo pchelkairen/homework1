c80eb2c44207eba8bfb64b0d9ac5c518a7732f7c
Irina <pamir.08@mail.ru>
2022-10-07 15:21:50 +0300

    ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write ("Введите номер дня недели: ");
+string number = Console.ReadLine ();
+
+if (number == "1")
+{
+Console.WriteLine ("Понедельник");
+}
+
+if (number == "2")
+{
+Console.WriteLine ("Вторник");
+}
+
+if (number == "3")
+{
+Console.WriteLine ("Среда");
+}
+
+if (number == "4")
+{
+Console.WriteLine ("Четверг");
+}
+
+if (number == "5")
+{
+Console.WriteLine ("Пятница");
+}
+
+if (number == "6")
+{
+Console.WriteLine ("Суббота");
+}
+
+if (number == "7")
+{
+Console.WriteLine ("Воскресенье");
+}
