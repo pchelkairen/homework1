7e9b07f8bc2d002c87331eb2bbc1235296c582e1
Irina <pamir.08@mail.ru>
2022-10-07 17:15:24 +0300

    ex7

---
+++ ex7/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random () .Next (99, 1000);
+Console.WriteLine (number);
+
+int a = (number % 100 - number % 10) / 10;
+Console.WriteLine ("Вторая цифра трёхзначного числа: ");
+Console. Write (a);
