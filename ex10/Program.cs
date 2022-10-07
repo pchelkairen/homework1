4b05271846b814aa16a52dfe5c02fec0d4a56a68
Irina <pamir.08@mail.ru>
2022-10-07 18:19:09 +0300

    ex10

---
+++ ex10/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = new Random () .Next (1, 99999);
+Console.WriteLine (a);
+
+int b = 5;
+Console.WriteLine (b);
+
+if (a % b == 0)
+{
+    Console.WriteLine ("Кратно");
+}
+
+else
+{
+    Console.Write ("Не кратно, остаток ");
+    int c = a % b;
+    Console.Write (c);
+}
