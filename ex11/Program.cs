d21723b5586a1f018b33e10f172cb895af54b78c
Irina <pamir.08@mail.ru>
2022-10-07 18:36:00 +0300

    ex11

---
+++ ex11/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = new Random () .Next (1, 999);
+Console.WriteLine (a);
+
+if (a >= 100)
+{
+    int b = a % 10;
+    Console.Write (" Третья цифра числа ");
+    Console.Write (b);
+}
+
+else
+{ 
+    Console.Write ("Третьей цифры числа нет");
+}
